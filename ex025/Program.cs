51d7bdcdd9b2deb895bf3f1f41ed65e546948dff
lowrider48rus <lowrider48rus@gmail.com>
2022-09-27 22:10:33 +0300

    ex025

---
+++ ex025/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.WriteLine("Введи число А: ");
+
+int A = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введи число B: ");
+
+int B = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine();
+
+Console.WriteLine($"Результат возведения числа {A} в степень {B} {Method1(A,B)}");
+
+int Method1(int num1, int num2)
+{
+    int result = 1;
+    for (int i = 0; i < num2; i++)
+    {
+        result = result * num1;
+    }
+    return result;
+}
